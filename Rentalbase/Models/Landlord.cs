caab44d8d2945f4014ab08dd2da4891f64cd8463
Michael Quandt <mdquandt@hotmail.com>
2018-03-06 10:58:16 -0800

    Annotations for Landlord

--- Rentalbase/Models/Landlord.cs
+++ Rentalbase/Models/Landlord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,26 @@ namespace Rentalbase.Models
     public class Landlord
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Full Name")]
         public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Street { get; set; }
+        [Required]
+        [StringLength(100)]
         public string City { get; set; }
+        [Required]
+        [StringLength(2)]
         public string State { get; set; }
+        [Required]
+        [Range(10000,99999)]
         public int Zip { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         public virtual ICollection<Property> Properties { get; set; }
