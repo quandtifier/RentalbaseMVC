e97453829570a9760fdc2d51ad739b7f0b4958ee
Michael Quandt <mdquandt@hotmail.com>
2018-03-06 11:54:58 -0800

    Annotated Tenant

--- Rentalbase/Models/Invoice.cs
+++ Rentalbase/Models/Invoice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 namespace Rentalbase.Models
@@ -8,9 +9,19 @@ namespace Rentalbase.Models
     {
         public int ID { get; set; }
         public int PropertyID { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Issue Date")]
         public DateTime DateIssued { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Payment Date")]
         public DateTime DatePaid { get; set; }
         public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Amount")]
         public decimal Cost { get; set; }
 
         public virtual InvoiceType InvoiceType { get; set; }
