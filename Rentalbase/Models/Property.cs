8e8cec84572985315c7c9f7b0cbb7daa3dff831a
Michael Quandt <mdquandt@hotmail.com>
2018-03-06 11:04:11 -0800

    Annotations for Property

--- Rentalbase/Models/Property.cs
+++ Rentalbase/Models/Property.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,19 @@ namespace Rentalbase.Models
     public class Property
     {
         public int ID { get; set; }
+        [Required]
         public int LandlordID { get; set; } = 1;
+        [Required]
+        [StringLength(100)]
         public string Street { get; set; }
+        [Required]
+        [StringLength(100)]
         public string City { get; set; }
+        [Required]
+        [StringLength(2)]
         public string State { get; set; }
+        [Required]
+        [Range(10000,99999)]
         public int Zip { get; set; }
         public int Value { get; set; }
         public string Description { get; set; }
