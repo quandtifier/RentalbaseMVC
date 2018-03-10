23454f37ef7b5869aba4cd8ec50bc8974c4d52f4
quandtifier <quandtifier@gmail.com>
2018-03-10 11:23:32 -0800

    Test class for Landlord added.

---
+++ Rentalbase.Tests/Controllers/LandlordControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rentalbase;
+using Rentalbase.Controllers;
+using Rentalbase.ViewModels;
+using Rentalbase.Models;
+using Rentalbase.DAL;
+
+namespace Rentalbase.Tests.Controllers
+{
+    [TestClass]
+    public class LandlordControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var controller = new LandlordController();
+
+            // Act
+            var result = controller.Index(null,null) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
